cc1f26db24912ee12b856e94cda64778edff45c6
sebashtioon <sebastiansuciu607@gmail.com>
2025-04-16 12:12:35 +1000

    init

--- src/singletons/[manager] encryption.gd
+++ src/singletons/[manager] encryption.gd
@@ -1,1 +1,19 @@
 extends Node
+
+func aes_encrypt(key: PackedByteArray, iv: PackedByteArray, plaintext: PackedByteArray) -> PackedByteArray:
+	var aes := AESContext.new()
+	if not aes.start(AESContext.MODE_CBC_ENCRYPT, key, iv):
+		push_error("Failed to start AES encryption")
+		return PackedByteArray()
+	var encrypted := aes.update(plaintext)
+	aes.finish()
+	return encrypted
+
+func aes_decrypt(key: PackedByteArray, iv: PackedByteArray, ciphertext: PackedByteArray) -> PackedByteArray:
+	var aes := AESContext.new()
+	if not aes.start(AESContext.MODE_CBC_DECRYPT, key, iv):
+		push_error("Failed to start AES decryption")
+		return PackedByteArray()
+	var decrypted := aes.update(ciphertext)
+	aes.finish()
+	return decrypted
